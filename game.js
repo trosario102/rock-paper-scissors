d48c91ff919e132dc046a6dac310d21bcb3824db
trosario102 <t.rosario102@gmail.com>
2022-02-09 16:15:57 -0600

    created playerPlay() to game.js

--- game.js
+++ game.js
@@ -3,3 +3,7 @@ function computerPlay() {
     let computerSelection = possiblePlays[Math.floor(Math.random() * possiblePlays.length)];
     return computerSelection;
 }
+
+function playerPlay() {
+    let playerSelection = prompt('Please enter rock, paper, or scissors:');
+}
