fa97b96b8ce92bb72e7c66743c95d9d2ecd611bd
trosario102 <t.rosario102@gmail.com>
2022-02-09 16:11:45 -0600

    forgot to add return statement in computerPlay function in game.js

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 function computerPlay() {
     let possiblePlays = ['rock', 'scissors', 'paper'];
     let computerSelection = possiblePlays[Math.floor(Math.random * possiblePlays.length)];
+    return computerSelection;
 }
